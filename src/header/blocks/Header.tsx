cd5f9f356c4ea779abd09bd013559a09c932826c
HaThanh12 <tha455420@gmail.com>
2025-08-12 14:34:09 +0700

    fix bug

--- src/header/blocks/Header.tsx
+++ src/header/blocks/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="topnav ">
+    <header className="topnav">
       <div className="container flex flex-row items-center justify-between px-[1.5rem] py-[0.5em] sticky bg-white shadow-lg shadow-gray-150">
         <Logo />
 
