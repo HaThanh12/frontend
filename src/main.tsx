272e80be6de789e683e56d3ced5dadf9ac554710
thanhsonnee <thanhsonjk16@gmail.com>
2025-08-12 15:49:25 +0700

    2nd commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import DynamicItems from './footer/dynamicItems.tsx'
+import Logos from './footer/logos.tsx'
+import Footer from './footer/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='main-container'>
+      <App />
+      <DynamicItems/>
+      <Logos/>
+      <Footer/>
+    </div>
   </StrictMode>,
 )
